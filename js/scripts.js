66694641880cfa3387487dec5cb73d860978c9b8
Jacob Knecht <jacobknecht3@gmail.com>
2019-05-06 16:13:24 -0500

    added helper function to set multiple attributes at once in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,25 @@
 *
 *  ==============================================*/
 
+const body = document.querySelector('body');
+const searchContainer = document.querySelector('.search-conntainer');
+const gallery = document.querySelector('.gallery');
+const modalContainer = document.createElement('div');
+
+modalContainer.setAttribute('class', 'modal-container');
+
 /* ================================================
 *
 *  HELPER FUNCTIONS
 *
 *  ==============================================*/
+
+//function to set multiple attributes at once
+function setAttributes(element, list) {
+  for(let attribute in list) {
+    element.setAttribute(attribute, list[attribute]);
+  }
+}
 
 /* ================================================
 *
@@ -16,11 +30,15 @@
 *
 *  ==============================================*/
 
+
+
 /* ================================================
 *
 *  GALLERY MARKUP GENERATION
 *
 *  ==============================================*/
+
+
 
 /* ================================================
 *
@@ -28,11 +46,15 @@
 *
 *  ==============================================*/
 
+
+
 /* ================================================
 *
 *  RANDOM USER GENERATOR API REQUEST LOGIC
 *
 *  ==============================================*/
+
+
 
 /* ================================================
 *
